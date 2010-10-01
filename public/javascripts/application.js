c8489c11b7f97d3ea7882b4e717544e2dcc149a2
Javier Álvarez Medina <jmedina@Javier-Alvarez-Medinas-MacBook-Pro.local>
2010-10-01 14:09:31 +0200

    posts page finished

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(function(){
+				$.fn.supersized.options = {  
+					startwidth: 936,  
+					startheight: 1036,
+					vertical_center: 1,
+					slideshow: 0
+				};
+				$('#supersized').supersized(); 
+		});
+});
