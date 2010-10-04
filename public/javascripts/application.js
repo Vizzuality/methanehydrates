b089c9ca65e2a0c93ea3064d87eaed013d776af1
Javier Álvarez Medina <jmedina@Javier-Alvarez-Medinas-MacBook-Pro.local>
2010-10-04 16:14:25 +0200

    last changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
     
+		//Cover ul space with blue opaque background (header)
+		var li_size = 0;
+		$('div.inner_header ul li').each(function(index,element){
+			li_size = li_size + $(element).width();
+		});
+		$('div.inner_header ul').css('background-position',(704-li_size-528-135) + 'px 0px');
+
+
+		
 		$(function(){
 				$.fn.supersized.options = {  
-					startwidth: 936,  
-					startheight: 1024,
+					startwidth: 1021,  
+					startheight: 901,
 					vertical_center: 1,
 					slideshow: 0
 				};
