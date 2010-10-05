e3eb70e85a2020fe9d2ee359c7a10ae0dcbb9bb7
Javier Álvarez Medina <jmedina@Javier-Alvarez-Medinas-MacBook-Pro.local>
2010-10-05 11:56:09 +0200

    last changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
 
   include Refinery::ApplicationHelper
+  
+  def to_ordinal(d)
+    if d == 1
+      "#{d}st"
+    elsif d == 2
+      "#{d}nd"
+    elsif d == 3
+      "#{d}rd"
+    else
+      "#{d}th"
+    end
+  end
+  
 
 end
