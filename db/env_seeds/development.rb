e1639948044664d1b16888acc8e47725f023a7da
Fernando Blat <ferblape@gmail.com>
2010-10-28 11:41:48 +0200

    Descripcion de la galeria

--- db/env_seeds/development.rb
+++ db/env_seeds/development.rb
@@ -11,6 +11,47 @@ e1 = Event.create :title => "Meeting #1", :description => "<p>Text for the meeti
 e2 = Event.create :title => "Meeting #2", :description => "<p>Text for the meeting #2</p><p>Other paragraph</p>", :from => Date.today, :to => Date.today + 1.day, :location => 'Hortaleza st, 48, Spain'
 e3 = Event.create :title => "Meeting #3", :description => "<p>Text for the meeting #3</p><p>Other paragraph</p>", :from => Date.today + 2.days, :to => Date.today + 4.days, :location => 'Hortaleza st, 48, Spain'
 
+images = []
+# Create 9 images
+1.upto(9) do |i|
+  images << Image.create(:image => File.open("#{Rails.root}/db/env_seeds/#{i}.jpg", 'rb'))
+end
+
+data_directory = "#{Rails.root}/doc/"
+filename = "methane.csv"
+FasterCSV.foreach(data_directory + '/' + filename) do |line|
+  next if line[0] == 'Geographic Location'
+  g = Gallery.create :name => line[3], :body => ''
+  f = Feature.new
+  f.region = line[0]
+  if ["Costa Rica", "Guatemala", "Japan", "Mexico", "Peru"].include?(f.region.split('-').last.strip)
+    f.country = f.region.split('-').last.strip
+    f.region = f.region.split('-')[0..-2].join('-')
+  end
+  f.primary_institution_name = line[1]
+  f.expedition = line[2]
+  f.title = line[3]
+  f.the_geom = "POINT(#{line[7].gsub(/,/,'.')} #{line[6].gsub(/,/,'.')})"
+  f.water_depth = line[8]
+  f.hydrate_depth = line[9]
+  f.hydrate_description = line[10]
+  f.references = line[11]
+  f.gallery_id = g.id
+  if f.save
+    puts " successfully created #{f.title}"
+  else
+    puts " errors creating feature: #{f.errors.full_messages}"
+  end
+end
+
+Gallery.all.each do |gallery|
+  i = rand(images.size - 1)
+  gallery.gallery_entries.create :name => "Image for gallery #{gallery.name} #{i}", :image_id => images[i].id
+  gallery.gallery_entries.create :name => "Image for gallery #{gallery.name} #{i+1}", :image_id => images[i+1].id
+end
+
+
+
 #
 # %W{ North\ America South\ America Asia Europe Africa Oceania }.each_with_index do |name, i|
 #
