1e11e43f8702907b2d8ebd3bcbc7dbdc6e06ed17
Fernando Espinosa <fer@ferdev.com>
2011-05-10 10:19:01 +0200

    Adapts deployment script for the new production environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,9 @@ role :app, linode_staging
 role :web, linode_staging
 role :db,  linode_staging, :primary => true
 
+set :user,  'ubuntu'
+set :scm_user, 'ubuntu'
+
+set :deploy_to, "/home/ubuntu/www/#{application}"
+
 set :branch, "staging"
